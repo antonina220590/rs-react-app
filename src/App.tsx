a4c5a46c979504c9d30a8526d5b8dbd16b3fdd3a
antonina220590 <tonya2290@gmail.com>
2025-02-01 23:19:16 +0300

    feat: update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { Component } from 'react';
 import './App.css';
 import SearchPage from './components/searchPage/searchPage';
 import ErrorBoundary from './utils/errorBoundary';
 
-function App() {
-  return (
-    <div className="min-h-screen flex flex-col items-center bg-[#bab2b5] bg-cover ">
-      <ErrorBoundary>
-        <SearchPage />
-      </ErrorBoundary>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="min-h-screen flex flex-col items-center bg-[#bab2b5] bg-cover ">
+        <ErrorBoundary>
+          <SearchPage />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
