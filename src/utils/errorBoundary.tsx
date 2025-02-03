c3530d37075592ab06005294d8a5399112204755
antonina220590 <tonya2290@gmail.com>
2025-02-03 13:46:51 +0300

    feat: change design of error page

--- src/utils/errorBoundary.tsx
+++ src/utils/errorBoundary.tsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import { IProps, IState } from './interface';
-import Error404Page from '../components/404Page/404page';
 
 class ErrorBoundary extends Component<IProps, IState> {
   constructor(props: IProps) {
@@ -21,7 +20,18 @@ class ErrorBoundary extends Component<IProps, IState> {
   render() {
     const { hasError } = this.state;
     if (hasError) {
-      return <Error404Page />;
+      return (
+        <div className="flex flex-col w-[1320px] mx-auto bg-gray-500/70 backdrop-blur-2xl border border-white/18 rounded-xl min-h-screen items-center justify-center gap-8 shadow-xl shadow-gray-700/20">
+          <h1>Something went wrong...</h1>
+          <button
+            className="w-[150px] h-[50px] cursor-pointer rounded-md bg-[#5555ff] text-white text-2xl border-none"
+            type="button"
+            onClick={this.refreshPage}
+          >
+            Reload Page
+          </button>
+        </div>
+      );
     }
     const { children } = this.props;
     return children;
