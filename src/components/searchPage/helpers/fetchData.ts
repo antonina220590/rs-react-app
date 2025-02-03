c3d0b4aeebadc27aafd50b1ac16769aec7e10e04
antonina220590 <tonya2290@gmail.com>
2025-02-03 18:19:58 +0300

    feat: migrate spinner component to functional component

--- src/components/searchPage/helpers/fetchData.ts
+++ src/components/searchPage/helpers/fetchData.ts
@@ -1,28 +1,26 @@
 import { getApiData } from '../../../utils/api';
 import { Character } from '../../../utils/interface';
 
-export default function fetchData(
+async function fetchData(
   searchQuery: string = '',
   setCharacters: React.Dispatch<React.SetStateAction<Character[]>>,
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
   setErrorMessage: React.Dispatch<React.SetStateAction<string | null>>
 ) {
-  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
   setIsLoading(true);
   setErrorMessage(null);
-  getApiData(searchQuery)
-    .then((data) => {
-      delay(300).then(() => {
-        setCharacters(data.results || []);
-        setIsLoading(false);
-      });
-    })
-    .catch((error) => {
-      const errorMessage =
-        error instanceof Error
-          ? error.message
-          : 'An unexpected error occurred.';
-      setErrorMessage(errorMessage);
-      setIsLoading(false);
-    });
+  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+  try {
+    const data = await getApiData(searchQuery);
+    await delay(300);
+    setCharacters(data.results || []);
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'An unexpected error occurred.';
+    setErrorMessage(errorMessage);
+  } finally {
+    setIsLoading(false);
+  }
 }
+
+export default fetchData;
--- src/components/spinner/spinners.tsx
+++ src/components/spinner/spinners.tsx
@@ -1,18 +1,14 @@
-import { Component } from 'react';
-
-class Spinner extends Component {
-  render() {
-    return (
-      <div
-        className="inline-block h-70 w-70 animate-spin rounded-full border-10 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-        role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
-    );
-  }
+function Spinner() {
+  return (
+    <div
+      className="inline-block h-70 w-70 animate-spin rounded-full border-10 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+      role="status"
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
+  );
 }
 
 export default Spinner;
