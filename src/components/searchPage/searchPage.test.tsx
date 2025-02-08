4aac35c1c93bae70921651619f95401b2131fe05
antonina220590 <tonya2290@gmail.com>
2025-02-08 21:25:40 +0300

    feat: add tests for searchPage

--- src/components/searchPage/searchPage.test.tsx
+++ src/components/searchPage/searchPage.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 import SearchPage from './searchPage';
 import { MemoryRouter } from 'react-router';
+import fetchData from './helpers/fetchData';
 
 const mockApiResponse = {
   info: {
@@ -33,7 +34,11 @@ const mockApiResponse = {
 
 vi.mock('../../utils/localStorage', () => ({
   useSearchQuery: vi.fn().mockReturnValue(['', vi.fn()]),
-  useSearchParams: vi.fn().mockReturnValue([new URLSearchParams(''), vi.fn()]),
+}));
+
+vi.mock('react-router-dom', () => ({
+  ...vi.importActual('react-router-dom'),
+  useSearchParams: vi.fn(),
 }));
 
 vi.mock('./helpers/fetchData', () => {
@@ -45,13 +50,11 @@ vi.mock('./helpers/fetchData', () => {
 
 describe('SearchPage', () => {
   beforeEach(() => {
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      json: () => Promise.resolve(mockApiResponse),
-    } as Response);
+    vi.mocked(fetchData).mockResolvedValue(mockApiResponse);
   });
 
   afterEach(() => {
-    vi.restoreAllMocks();
+    vi.clearAllMocks();
   });
 
   it('renders the search input', () => {
@@ -61,5 +64,37 @@ describe('SearchPage', () => {
       </MemoryRouter>
     );
     expect(screen.getByTestId('inputElement')).toBeInTheDocument();
+  });
+
+  it('displays loading spinner while fetching data', () => {
+    render(
+      <MemoryRouter>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+  });
+
+  it('displays loading spinner while fetching data', () => {
+    render(
+      <MemoryRouter>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+  });
+
+  it('renders character cards after data is loaded', async () => {
+    (fetchData as jest.Mock).mockResolvedValue(mockApiResponse);
+
+    render(
+      <MemoryRouter>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
   });
 });
