3895d21ba5799384c22e687e74a11ca44da7c969
antonina220590 <tonya2290@gmail.com>
2025-03-17 08:43:59 +0300

    feat: set up the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  const a = 0;
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1 className="text-amber-600">I am your main page</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
